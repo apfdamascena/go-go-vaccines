aa71229be82ead265e54b846813d17093a711c14
igrphillipe <igrphillipe@gmail.com>
2021-04-18 21:36:45 -0300

    feature(moving): add main character to images class

--- Images.py
+++ Images.py
@@ -4,14 +4,18 @@ import pygame
 
 class Images:
 
-    IMAGE_BOTTOM_CITY = os.path.join('assets', 'background', 'bottom_background_city.png')
+    IMAGE_BOTTOM_CITY = os.path.join(
+        'assets', 'background', 'bottom_background_city.png')
     STARS = os.path.join('assets', 'background', 'stars.png')
-    IMAGE_TOP_CITY = os.path.join('assets', 'background', 'top_background_without_stars.png')
+    IMAGE_TOP_CITY = os.path.join(
+        'assets', 'background', 'top_background_without_stars.png')
+    MAIN_CHARACTER = os.path.join('assets', 'characters', 'main-character.png')
 
     def __init__(self):
         self.__bottom_city = pygame.image.load(self.IMAGE_BOTTOM_CITY)
         self.__top_city = pygame.image.load(self.IMAGE_TOP_CITY)
         self.__starts = pygame.image.load(self.STARS)
+        self.__main_character = pygame.image.load(self.MAIN_CHARACTER)
 
     @property
     def bottom_city(self):
@@ -24,3 +28,7 @@ class Images:
     @property
     def stars(self):
         return self.__starts
+
+    @property
+    def main_character(self):
+        return self.__main_character
