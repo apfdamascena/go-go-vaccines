49d7209c6650735b253481da5650478f0d96c0f4
igrphillipe <igrphillipe@gmail.com>
2021-04-22 08:45:08 -0300

    feature(random-obstacle): add obstacles to the game

--- Game.py
+++ Game.py
@@ -3,6 +3,9 @@ from constants.PlayerConstants import PlayerConstants
 from background.MovingBackground import MovingBackground
 from background.VaccineBackground import VaccineBackground
 from player.Player import Player
+from obstacle.Box import Box
+from obstacle.Boxes import Boxes
+from obstacle.Obstacles import Obstacles
 import pygame
 
 
@@ -13,6 +16,9 @@ class Game:
         self.__screen = pygame.display.set_mode((1000, 700))
         self.__player = Player()
         self.__vaccines_background = VaccineBackground()
+        self.__Box = Box()
+        self.__Boxes = Boxes()
+        self.__Obstacles = Obstacles()
         pygame.init()
 
     def play(self):
@@ -24,6 +30,13 @@ class Game:
             self.__player.draw(self.__screen, axis_x, axis_y)
             axis_x, axis_y = self.__player.move(axis_x, axis_y)
 
+            self.__Obstacles.drawObstacle(self.__screen)
+
+
+"""             self.__Box.draw(self.__screen)
+            self.__Box.move()
+            self.__Boxes.draw(self.__screen)
+            self.__Boxes.move() """
 
 
 if __name__ == "__main__":
