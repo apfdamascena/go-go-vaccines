04c7ada4ff048565314674bae3c2b6b92d605b59
Sofiamdl <sml#cin.ufpe.br>
2021-04-23 08:28:07 -0300

    feature/walk

--- obstacle/Box.py
+++ obstacle/Box.py
@@ -3,29 +3,30 @@ import random
 from images.ObstacleAssets import ObstacleAssets
 from constants.BackgroundConstants import BackgroundConstants
 
-
+# 900 no x
+# 580 no y
 class Box:
 
-    def __init__(self):
+    def __init__(self, axis_x, axis_y):
         self.__images = ObstacleAssets()
         self.__mult = 1
-        self.__axis_begin = 900
-        self.__axis_y =  580
+        self.__axis_x = axis_x
+        self.__axis_y =  axis_y
 
     def draw(self, window):
         window.blit(self.__images.box_obstacle,
-                    (self.__axis_begin, 580, 100, 100))           
+                    (self.__axis_x, self.__axis_y, 100, 100))           
         #pygame.display.update()
 
     def move(self):
-        self.__axis_begin -= BackgroundConstants.VELOCITY * self.__mult
+        self.__axis_x -= BackgroundConstants.VELOCITY * self.__mult
 
-        if self.__axis_begin < BackgroundConstants.WIDTH * BackgroundConstants.AXIS_ADJUSTMENT:
-            self.__axis_begin = 900 + random.randint(200, 900)
+        if self.__axis_x < BackgroundConstants.WIDTH * BackgroundConstants.AXIS_ADJUSTMENT:
+            self.__axis_x = 900 + random.randint(200, 900)
 
     @property
     def axis_x(self):
-        return self.__axis_begin
+        return self.__axis_x
     
     @property
     def axis_y(self):
---
+++ obstacle/LShapedObstacle.py
@@ -0,0 +1,39 @@
+
+from images.ObstacleAssets import ObstacleAssets
+from obstacle.Box import Box
+
+class LShapedObstacle:
+
+    def __init__(self):
+        self.__images = ObstacleAssets()
+        self.__shape = [
+            [1,1,0],
+            [0,1,0],
+            [0,0,0]
+        ]
+        self.__obstacles = []
+        self.__create()
+    
+    def __create(self):
+        for x in range(3):
+            line_obstacles = []
+            for y in range(3):
+                if self.__shape[y][x] == 1:
+                    line_obstacles.append(
+                        Box(900+90*x, 580-90*y))
+                else:
+                    line_obstacles.append(None)
+            self.__obstacles.append(line_obstacles)
+
+    def draw(self, window):
+        for x in range(3):
+            for y in range(3):
+                if self.__obstacles[x][y] != None:
+                    self.__obstacles[x][y].draw(window)
+
+    def move(self):
+        for x in range(3):
+            for y in range(3):
+                if self.__obstacles[x][y] != None:
+                    self.__obstacles[x][y].move()
+
