ee4b4f41939ff1c702f0cb9472d3e1eb59b9ba03
igrphillipe <igrphillipe@gmail.com>
2021-04-24 19:31:22 -0300

    feature(random-obstacle): create two random boxes

--- manager/ObstacleManager.py
+++ manager/ObstacleManager.py
@@ -1,15 +1,17 @@
+import random
 from obstacle.Box import Box
 from manager.Manager import Manager
+
 
 class ObstacleManager(Manager):
 
     def __init__(self):
+        first = random.randint(900, 1800)
+        second = random.randint(1800, 3600)
+
         self.__boxes = [
-            Box(900),
-            Box(1419),
-            Box(2100),
-            Box(2762),
-            Box(3200)
+            Box(first),
+            Box(second)
         ]
         super().__init__(self.__boxes)
 
