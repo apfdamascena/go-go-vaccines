8c6cae041eadca41eaf020fb5af9e2c0ea088e80
igrphillipe <igrphillipe@gmail.com>
2021-04-24 19:30:56 -0300

    feature(random-obstacle): fix identation

--- obstacle/Box.py
+++ obstacle/Box.py
@@ -4,20 +4,19 @@ from images.ObstacleAssets import ObstacleAssets
 from constants.BackgroundConstants import BackgroundConstants
 from constants.BoxConstants import BoxConstants
 
-# 900 no x
-# 580 no y
+
 class Box:
 
     def __init__(self, axis_x):
         self.__images = ObstacleAssets()
         self.__mult = 1
         self.__axis_x, self.__saved_axis_x = axis_x, axis_x
-        self.__axis_y =  BoxConstants.AXIS_Y
+        self.__axis_y = BoxConstants.AXIS_Y
 
     def draw(self, window):
         window.blit(self.__images.box_obstacle,
-                    (self.__axis_x, self.__axis_y, 100, 100))           
-        #pygame.display.update()
+                    (self.__axis_x, self.__axis_y, 100, 100))
+        # pygame.display.update()
 
     def move(self):
         self.__axis_x -= BackgroundConstants.VELOCITY * self.__mult
@@ -28,7 +27,7 @@ class Box:
     @property
     def axis_x(self):
         return self.__axis_x
-    
+
     @property
     def axis_y(self):
         return self.__axis_y
